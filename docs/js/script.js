9e9af0e2c68a81ba3f912a7e4033b3be52fe3af1
s20203079 <s20203079@HCS20039.stdlan.hcs.local>
2021-05-28 09:44:47 +0900

    いいね！の追加

--- docs/js/script.js
+++ docs/js/script.js
@@ -18,3 +18,10 @@ $(function () {
   });
 
 });
+
+$(function(){
+	$(document).one('click', '.like-review', function(e) {
+		$(this).html('<i class="fa fa-heart" aria-hidden="true"></i> You liked this');
+		$(this).children('.fa-heart').addClass('animate-like');
+	});
+});
